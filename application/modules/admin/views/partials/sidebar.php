5f25d5c370e291e906896bbca5b3c05deff93409
Sourabh Jain <jain.sourabh690@hotmail.com>
2017-07-23 12:25:24 +0530

    Url control navigation

--- application/modules/admin/views/partials/sidebar.php
+++ application/modules/admin/views/partials/sidebar.php
@@ -2,13 +2,13 @@
 <div id="sidebar">
   <a href="#" class="visible-phone"><i class="icon icon-th"></i>Tables</a>
   <ul style="display: block;">
-    <li>
-        <a href="<?= base_url('admin/index/home'); ?>" class="active"><i class="icon icon-home"></i> <span>Dashboard</span></a> 
+    <li <?= (in_array('home', uri_control()) ? 'class="active"': '') ?>>
+        <a href="<?= base_url('admin/index/home'); ?>"><i class="icon icon-home"></i> <span>Dashboard</span></a> 
     </li>
-    <li>
+    <li <?= (in_array('users', uri_control()) ? 'class="active"': '') ?>>
         <a href="<?= base_url('admin/users'); ?>"><i class="icon icon-user"></i> <span>Users</span></a> 
     </li>
-    <li> 
+    <li <?= (in_array('products', uri_control()) ? 'class="active"': '') ?>> 
         <a href="<?= base_url('admin/products'); ?>"><i class="icon-list-ul"></i> <span>Products</span></a> 
     </li>
   </ul>
